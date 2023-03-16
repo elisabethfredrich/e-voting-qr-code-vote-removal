154a5a3bf9792a5bbf78098be0803cd0ca3c93f2
Lara Elisabeth Fredrich <lfre@itu.dk>
2023-03-16 12:08:26 +0100

    update survey link

--- src/Voting-System/VoteVerification/IndividualVoteVerification.js
+++ src/Voting-System/VoteVerification/IndividualVoteVerification.js
@@ -101,7 +101,7 @@ return () => {
                       padding={"1rem"}
                       onClick={() =>
                         (window.location.href =
-                          "https://www.survey-xact.dk/LinkCollector?key=TC9S9SFFJPC5")
+                          "https://www.survey-xact.dk/LinkCollector?key=5SVF9X6XL695")
                       }
                     >
                       Go to survey
--- src/Voting-System/VoteVerification/VoteVerification.js
+++ src/Voting-System/VoteVerification/VoteVerification.js
@@ -250,7 +250,7 @@ export default function VoteVerification() {
                 padding={"1rem"}
                 onClick={() =>
                   (window.location.href =
-                    "https://www.survey-xact.dk/LinkCollector?key=TC9S9SFFJPC5")
+                    "https://www.survey-xact.dk/LinkCollector?key=5SVF9X6XL695")
                 }
               >
                 Go to survey
