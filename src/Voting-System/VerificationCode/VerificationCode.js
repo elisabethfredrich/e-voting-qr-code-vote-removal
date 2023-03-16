703ce3cf95d2176a37ae0458396a79fc2570fdc8
Lara Elisabeth Fredrich <lfre@itu.dk>
2023-03-16 12:25:30 +0100

    update pdf download voter id

--- src/Voting-System/VerificationCode/VerificationCode.js
+++ src/Voting-System/VerificationCode/VerificationCode.js
@@ -88,7 +88,7 @@ export default function VerificationCode() {
                     download
                   </span>
                 </Text>
-                {<PDFgenerator voterId={voter.id} />}
+                {<PDFgenerator voterId={voter.attributes.username} />}
               </Button>
             </Grid>
             <Checkbox
