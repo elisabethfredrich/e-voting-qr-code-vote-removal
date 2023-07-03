0754b188b5018361762c4eebcbaad8df62368f5e
Lara Elisabeth Fredrich <lfre@itu.dk>
2023-07-03 14:55:06 +0200

    add copyright and license notices as well as COPYING file for GNU General Public license

--- src/Voting-System/Reporting/ReportingConformation.js
+++ src/Voting-System/Reporting/ReportingConformation.js
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2023 Christina Frederikke Nissen, Elisabeth Fredrich
+ *
+ * This file is part of e-voting-system-auto-remove.
+ *
+ * e-voting-system-auto-remove is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * e-voting-system-auto-remove is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with e-voting-system-auto-remove. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React from "react";
 import { Text, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
